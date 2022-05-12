02af0843414831580f84ca91bfe6e846f3398dc6
QWERTYpy <burtsevmv@mail.ru>
2022-05-12 11:31:39 +0300

    Чистка кода

--- main.py
+++ main.py
@@ -1,41 +1,15 @@
 import tensorflow as tf
-from matplotlib.backends.backend_agg import FigureCanvasAgg
-from matplotlib.figure import Figure
 from tf2_yolov4.anchors import YOLOV4_ANCHORS
 from tf2_yolov4.model import YOLOv4
 import cv2
-import numpy as np
 import time
 from threading import Thread, active_count
 from collections import deque
 #################################################
 
 
-def load_image_to_tensor(file):
-    # load image
-    image = tf.io.read_file(file)
-    # detect format (JPEG, PNG, BMP, or GIF) and converts to Tensor:
-    image = tf.io.decode_image(image)
-    return image
-
-
-def resize_image(image):
-    # Resize the output_image:
-    image = tf.image.resize(image, (HEIGHT, WIDTH))
-    # Add a batch dim:
-    images = tf.expand_dims(image, axis=0)/255
-    return images
-
-
-def get_image_from_plot():
-    # crates a numpy array from the output_image of the plot\figure
-    canvas = FigureCanvasAgg(Figure())
-    canvas.draw()
-    return np.fromstring(canvas.tostring_rgb(), dtype='uint8')
-
-
 def trained_yolov4_model():
-    # load trained yolov4 model
+    # Загружаем обученную модель yolov4
     model = YOLOv4(
         input_shape=(HEIGHT, WIDTH, 3),
         anchors=YOLOV4_ANCHORS,
@@ -49,7 +23,8 @@ def trained_yolov4_model():
     return model
 
 
-def detected_photo(boxes, scores, classes, detections, image, image_all, time_detection):
+def detected_photo(boxes, scores, classes, detections, image, time_detection):
+    # Ищем на переданной фотографии заданные объекты
     boxes = (boxes[0] * [WIDTH, HEIGHT, WIDTH, HEIGHT]).astype(int)
     scores = scores[0]
     classes = classes[0].astype(int)
@@ -84,25 +59,29 @@ def detected_photo(boxes, scores, classes, detections, image, image_all, time_de
                 text = CLASSES[class_idx] + ': {0:.2f}'.format(score)
                 cv2.putText(image_cv, text, (int(xmin), int(ymin) - 5),
                             cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 0), 2)
-    if flag_detection:
-        #now_time = time.gmtime()
-        #str_time = f'{now_time.tm_mon}_{now_time.tm_mday}_{now_time.tm_hour}_{now_time.tm_min}_{now_time.tm_sec}'
-        cv2.imwrite(f'photos/{time_detection}.jpg', image_cv*255)
-    # return image_cv
+    return flag_detection, time_detection, image_cv
 
 
-def proccess_frame(model, image_all):
+def save_frame_as_photo(image, time_detection):
+    cv2.imwrite(f'photos/{time_detection}.jpg', image)
+
+
+def proccess_frame(model):
+    # Обрабатываем изображения из стека
     while len(memory) > 0:
-        #images = resize_image(photo)
         print('Размер стека: ', len(memory))
-        photo, time_detection = memory.popleft()
-        images = resize_image(photo)
+        images, time_detection = memory.popleft()
+        images = tf.image.resize(images, (HEIGHT, WIDTH))
+        # Add a batch dim:
+        images = tf.expand_dims(images, axis=0) / 255
+
         boxes, scores, classes, detections = model.predict(images)
-        detected_photo(boxes, scores, classes, detections, images[0], image_all, time_detection)
+        flag_detection, time_detection, image_cv = detected_photo(boxes, scores, classes, detections, images[0], time_detection)
+        if flag_detection:
+            save_frame_as_photo(image_cv*255, time_detection)
+
 
 # Функция вычисления хэша
-
-
 def CalcImageHash(FileName):
     # image = cv2.imread(FileName)  # Прочитаем картинку
     image_h = FileName
@@ -137,53 +116,52 @@ def compare_hash(hash1, hash2):
 
 
 def car_detection_video():
+    # Анализируем видео поток
     camera = cv2.VideoCapture('rtsp://admin:admin@10.64.130.10:554/h264')
     frame_width_det = (camera.get(cv2.CAP_PROP_FRAME_WIDTH))  # Получаем размер исходного видео
     frame_height_det = (camera.get(cv2.CAP_PROP_FRAME_HEIGHT))
     dim = (WIDTH, HEIGHT)
 
     name_window = 'IP Camera - 10.64.130.4'
-    # write resulted frames to file
-    #out = cv2.VideoWriter(output_video_name, cv2.VideoWriter_fourcc(*'MJPG'), 30, (WIDTH ,HEIGHT))
     success, image = camera.read()
+    # Обрезаем фрейм для детекции движения в определенной области
     image = image[int(frame_height_det)//4:int(frame_height_det), 0:int(frame_width_det)]
     image = cv2.resize(image, dim, interpolation=cv2.INTER_AREA)
     image_tmp = image
     while True:
-        # function extract frames
+        # Поочередно извлекаем фреймы из потока
+        # Пропускаем 5 шт для уменьшения нагрузки и детекции движения
         for __ in range(5):
            _, _ = camera.read()
         success, image2 = camera.read()
         image2_all = image2
-        image2 = image2[int(frame_height_det) // 4:int(frame_height_det), 0:int(frame_width_det)]
+        #image2 = image2[int(frame_height_det) // 4:int(frame_height_det), 0:int(frame_width_det)]
+        image2 = image2[int(frame_height_det)//4:int(frame_height_det), 0:int(frame_width_det)]
         image2 = cv2.resize(image2, dim, interpolation=cv2.INTER_AREA)
         diffimg = cv2.absdiff(image, image2)
         d_s = cv2.sumElems(diffimg)
         d = (d_s[0] + d_s[1] + d_s[2]) / (WIDTH * HEIGHT)
         image = image2
+        # Если активность больше заданного порога
         if d > 7:
+            # Сравниваем фреймы на похожесть, чтобы исключить избыточности
+            # Данный алгоритм работает коряво, но позволяет отфильтровать часть
             hash1 = CalcImageHash(image)
             hash2 = CalcImageHash(image_tmp)
             c_h = compare_hash(hash1, hash2)
+            # Если различие выше порога
             if c_h > 12:
                 image_tmp = image
                 cv2.namedWindow(name_window, 0)
-                cv2.imshow(name_window, image2_all)
+                cv2.imshow(name_window, image2_all)  # Показываем оригинальный фрейм
                 cv2.resizeWindow(name_window, int(frame_width_det) // 2,
                                  int(frame_height_det) // 2)
 
-                memory.append([tf.convert_to_tensor(image), int(time.time() * 1000)])
-                print('Стек ADD:', len(memory))
-                print('Процессов :', active_count())
-                if active_count() == 1:
-                    Thread(target=proccess_frame, args=(model, image2_all)).start()
-                # Thread(target=proccess_frame,
-                #        args=(tf.convert_to_tensor(image), model, image2_all, int(time.time() * 1000))).start()
+                memory.append([tf.convert_to_tensor(image), int(time.time() * 1000)])  # Запоминаем его
+                print('Стек ADD:', len(memory))  # Выводим количство фреймов в стеке
+                if active_count() == 1:  # Если в данные момент запущен только один процесс, то запускаем обработку
+                    Thread(target=proccess_frame, args=(model,)).start()
 
-                cv2.namedWindow(name_window, 0)
-                cv2.imshow(name_window, image2_all)
-                cv2.resizeWindow(name_window, int(frame_width_det) // 2,
-                                 int(frame_height_det) // 2)  # Устанавливаем размер окна вывода
         if cv2.waitKey(2) == 27:  # Выход по ESC
             break
         if cv2.waitKey(1) & 0xFF == ord('q'):
@@ -195,7 +173,7 @@ def car_detection_video():
 
 if __name__ == "__main__":
 
-    WIDTH, HEIGHT = 512, 512
+    WIDTH, HEIGHT = int(1920/4), int(768/4)
     """
     width=608 height=608 in cfg: 65.7% mAP@0.5 (43.5% AP@0.5:0.95) - 34(R) FPS / 62(V) FPS - 128.5 BFlops
     width=512 height=512 in cfg: 64.9% mAP@0.5 (43.0% AP@0.5:0.95) - 45(R) FPS / 83(V) FPS - 91.1 BFlops
@@ -204,6 +182,6 @@ if __name__ == "__main__":
     """
 
 ####   Detect Cars on a video ######
-    model = trained_yolov4_model()
-    memory = deque()
-    car_detection_video()
+    model = trained_yolov4_model()  # Загружаем обученную модель
+    memory = deque()  # Создаем стек
+    car_detection_video()  # Запускаем детектор
